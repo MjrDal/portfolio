ef4df8af4cc0a6727f030910862f8f1d92e3adb7
MjrDal <mjrdal.39@gmail.com>
2024-03-26 16:25:06 +0100

    add skills

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -6,10 +6,22 @@ export const Skills = () => {
   return (
     <div className="font-sans mt-4">
       <Title title="My skills" />
-      <div>
-        <WheelSkills image1="" image2="" image3="" image4="" />
-        <WheelSkills image1="" image2="" image3="" image4="" />
-        <WheelSkills image1="" image2="" image3="" image4="" />
+      <div className="flex flex-row">
+        <WheelSkills
+          image1="/html-5.svg"
+          image2="css-3.svg"
+          image3="js-1.svg"
+          image4="typescript.svg"
+          title="Languages"
+        />
+        <WheelSkills
+          image1="react.svg"
+          image2="Vector.svg"
+          image3=""
+          image4=""
+          title="Framework"
+        />
+        <WheelSkills image1="" image2="" image3="" image4="" title="Other" />
       </div>
     </div>
   );
--- components/wheelSkills/WheelSkills.tsx
+++ components/wheelSkills/WheelSkills.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Image from "next/image";
 
 interface WheelSkillsProps {
   image1: string;
   image2: string;
   image3: string;
   image4: string;
+  title: string;
 }
 
 export const WheelSkills = ({
@@ -12,16 +14,26 @@ export const WheelSkills = ({
   image2,
   image3,
   image4,
+  title,
 }: WheelSkillsProps) => {
   return (
-    <div className="font-sans mt-4">
+    <div className="font-sans mt-4 w-[380px] h-[380px] relative flex justify-center items-center rounded-full">
       <div className="w-[300px] h-[300px] flex justify-center items-center rounded-full bg-orange">
-        <div className="w-[280px] h-[280px] rounded-full bg-black">
-          <div className="w-[80px] h-[80px] rounded-full bg-grayLight"></div>
-          <div className="w-[80px] h-[80px] rounded-full bg-grayLight"></div>
-          <div className="w-[80px] h-[80px] rounded-full bg-grayLight"></div>
-          <div className="w-[80px] h-[80px] rounded-full bg-grayLight"></div>
+        <div className="w-[280px] h-[280px] flex justify-center items-center rounded-full bg-black text-white text-2xl">
+          {title}
         </div>
+      </div>
+      <div className="w-[80px] h-[80px] absolute bottom-0 rounded-full flex justify-center items-center bg-grayLight">
+        <Image src={image1} width={50} height={50} alt="Image computer" />
+      </div>
+      <div className="w-[80px] h-[80px] absolute left-0 rounded-full flex justify-center items-center bg-grayLight">
+        <Image src={image2} width={50} height={50} alt="Image computer" />
+      </div>
+      <div className="w-[80px] h-[80px] absolute right-0 rounded-full flex justify-center items-center bg-grayLight">
+        <Image src={image3} width={50} height={50} alt="Image computer" />
+      </div>
+      <div className="w-[80px] h-[80px] absolute top-0 rounded-full flex justify-center items-center bg-grayLight">
+        <Image src={image4} width={50} height={50} alt="Image computer" />
       </div>
     </div>
   );
