791ca1f93a2a4c97c2bbc0997d49fe3cfda99f1a
MjrDal <mjrdal.39@gmail.com>
2024-03-26 10:22:20 +0100

    add separator

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Presentation } from "@/components/presentation/Presentation";
+import { Separator } from "@/components/separator/Separator";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+    <main className="flex min-h-screen flex-col items-center p-24 bg-black">
       <Presentation />
+      <Separator />
     </main>
   );
 }
---
+++ components/separator/Separator.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Separator = () => {
+  return (
+    <div className=" flex flex-row w-full mt-40 gap-6">
+      <div className="w-2 h-2 rounded-full bg-orange"></div>
+      <div className="w-2 h-2 rounded-full bg-orange"></div>
+      <div className="w-full h-2 rounded-full bg-orange"></div>
+    </div>
+  );
+};
