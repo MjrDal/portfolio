0cd99a52242872f6140703c78600e23e9012514a
MjrDal <mjrdal.39@gmail.com>
2024-03-27 15:24:15 +0100

    add btn all projects

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,12 +1,14 @@
 "use client";
+import { FaAngleRight } from "react-icons/fa";
 import { CardHome } from "../cardHome/CardHome";
 import { Title } from "../title/Title";
+import { Button } from "../ui/button";
 
 export const Projects = () => {
   return (
     <div className="font-sans mt-4">
       <Title title="My projects" />
-      <div className="flex flex-row">
+      <div className="flex flex-row mb-20">
         <CardHome
           titleLink="See Kasa code"
           link="https://github.com/MjrDal/Kasa"
@@ -34,6 +36,12 @@ export const Projects = () => {
           image="/sophiebluel.PNG"
           alt="image du site sophie bluel"
         />
+      </div>
+      <div className="flex justify-end p-4">
+        <Button className="bg-orange text-white py-8">
+          <p className="text-xl">All projects</p>
+          <FaAngleRight className="text-5xl" />
+        </Button>
       </div>
     </div>
   );
