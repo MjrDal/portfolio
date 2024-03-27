66697a3f3d7792b6515f7ee063989c373c889536
MjrDal <mjrdal.39@gmail.com>
2024-03-27 16:17:51 +0100

    save 27/03/2024

--- app/(landing)/projects/page.tsx
+++ app/(landing)/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function ProjectsPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-black">
-      page des peojets
+      page des projets
     </main>
   );
 }
