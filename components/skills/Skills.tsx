8d16efb28bfec99da939cdd2865883b8d2cee25c
MjrDal <mjrdal.39@gmail.com>
2024-03-28 13:15:07 +0100

    fix css skills

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -6,28 +6,32 @@ export const Skills = () => {
   return (
     <div className="font-sans mt-4">
       <Title title="My skills" />
-      <div className="flex flex-row">
-        <WheelSkills
-          image1="/html-5.svg"
-          image2="css-3.svg"
-          image3="js-1.svg"
-          image4="typescript.svg"
-          title="Languages"
-        />
-        <WheelSkills
-          image1="react.svg"
-          image2="Vector.svg"
-          image3="/tailwind.png"
-          image4="/materialize.png"
-          title="Framework"
-        />
-        <WheelSkills
-          image1="/postgressql.png"
-          image2="/redux.png"
-          image3="/sass.png"
-          image4="/gihub.png"
-          title="Other"
-        />
+      <div className="flex flex-col justify-center mt-4 mb-8">
+        <div className=" flex flex-row gap-8 ">
+          <WheelSkills
+            image1="/html-5.svg"
+            image2="css-3.svg"
+            image3="js-1.svg"
+            image4="typescript.svg"
+            title="Languages"
+          />
+          <WheelSkills
+            image1="react.svg"
+            image2="Vector.svg"
+            image3="/tailwind.png"
+            image4="/materialize.png"
+            title="Framework"
+          />
+        </div>
+        <div className=" flex justify-center ">
+          <WheelSkills
+            image1="/postgressql.png"
+            image2="/redux.png"
+            image3="/sass.png"
+            image4="/gihub.png"
+            title="Other"
+          />
+        </div>
       </div>
     </div>
   );
