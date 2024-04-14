547559af533d83b059e5d5346a9d8b28b2ee948a
MjrDal <mjrdal.39@gmail.com>
2024-04-14 11:38:51 +0200

    wrap root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={(inter.className, "h-full")}>{children}</body>
+      <body className={(inter.className, "h-full")}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
