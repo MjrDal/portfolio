75ec45d5de0cf5669cf7c79e3ef1f9ac99e6b163
MjrDal <mjrdal.39@gmail.com>
2024-04-16 08:05:07 +0200

    save

--- auth.ts
+++ auth.ts
@@ -1,5 +1,47 @@
+import { signInSchema } from "@/schemas";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { ZodError } from "zod";
+// Your own logic for dealing with plaintext password strings; be careful!
+import { getUserFromDb } from "@/utils/db";
+import { saltAndHashPassword } from "@/utils/password";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const { handlers, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          let user = null;
+
+          const { email, password } = await signInSchema.parseAsync(
+            credentials
+          );
+
+          // logic to salt and hash password
+          const pwHash = saltAndHashPassword(password);
+
+          // logic to verify if user exists
+          user = await getUserFromDb(email, pwHash);
+
+          if (!user) {
+            throw new Error("User not found.");
+          }
+
+          // return json object with the user data
+          return user;
+        } catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null;
+          }
+        }
+      },
+    }),
+  ],
 });
--- schemas/index.ts
+++ schemas/index.ts
@@ -10,3 +10,15 @@ export const LoginSchema = z.object({
   email: z.string().email({ message: "email is requered" }),
   password: z.string().min(2, { message: "message is requered" }),
 });
+
+export const signInSchema = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+});
