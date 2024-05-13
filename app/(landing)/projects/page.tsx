dd94cec3196676c0054edb0a98a638cf9cdd9ba8
MjrDal <mjrdal.39@gmail.com>
2024-05-13 11:14:25 +0200

    save

--- app/(landing)/projects/page.tsx
+++ app/(landing)/projects/page.tsx
@@ -1,12 +1,5 @@
 import { auth } from "@/auth";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { PrismaClient } from "@prisma/client";
 
 export default async function ProjectsPage() {
@@ -17,32 +10,6 @@ export default async function ProjectsPage() {
 
   return (
     <main className="flex flex-col items-center p-24 ">
-      <div className="">
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Theme" />
-          </SelectTrigger>
-          <SelectContent>
-            {tags.map((docs) => (
-              <SelectItem key={docs.id} value={docs.tag}>
-                {docs.tag}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="project" />
-          </SelectTrigger>
-          <SelectContent>
-            {project.map((docs) => (
-              <SelectItem key={docs.id} value={docs.id}>
-                {docs.title}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
       <div className="max-w-5xl mx-auto px-8">
         <HoverEffect
           project={project}
